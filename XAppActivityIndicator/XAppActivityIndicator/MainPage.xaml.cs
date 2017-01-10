7c918f0db61ae05942ad0f84deb6ef4fca3ee63a
Ian Chen <ian.msmvp@outlook.com>
2017-01-10 13:56:50 +0800

    add ActivityIndicator Demo

---
+++ XAppActivityIndicator/XAppActivityIndicator/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XAppActivityIndicator.ViewModels;
+
+namespace XAppActivityIndicator
+{
+    public partial class MainPage : ContentPage
+    {
+        private ActivityIndicatorViewModel ViewModel { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new ActivityIndicatorViewModel();
+            BindingContext = ViewModel;
+
+            this.BtnGetData.Clicked += async (sender, args) => await GetDataTaskAsync();
+        }
+
+        private async Task GetDataTaskAsync()
+        {
+            ViewModel.IsBusy = true;
+            await Task.Delay(3000);
+            ViewModel.IsBusy = false;
+        }
+    }
+}
---
+++ XAppActivityIndicator/XAppActivityIndicator/ViewModels/ActivityIndicatorViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAppActivityIndicator.ViewModels
+{
+    public class ActivityIndicatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {                                                                   
+                _isBusy = value;
+                PropertyChanged(this,new PropertyChangedEventArgs("IsBusy"));
+            }
+        }
+    }
+}
